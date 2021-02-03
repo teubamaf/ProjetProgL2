f6ca3f3e08bd35ef8ff9cde989524eb4534cca28
teubamaf <amaury.durand-noel@hotmail.fr>
2021-02-03 15:15:34 +0100

    First Commit

---
+++ src/app/loginsystem/loginsystem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginsystem',
+  templateUrl: './loginsystem.component.html',
+  styleUrls: ['./loginsystem.component.css']
+})
+export class LoginsystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
