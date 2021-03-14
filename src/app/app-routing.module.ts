49d203f62c0d81c9acf4b1856b35fee2ad4a04f5
Nelchel <nelchel@riseup.net>
2021-03-14 15:22:31 +0100

    Fini p2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
+// Required services for navigation
 import { RouterModule, Routes } from '@angular/router';
 
+// Import all the components for which navigation service has to be activated
+import { SignInComponent } from '././components/sign-in/sign-in.component';
+import { SignUpComponent } from '././components/sign-up/sign-up.component';
+import { DashboardComponent } from '././components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from '././components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from '././components/verify-email/verify-email.component';
+
+import { AuthGuard } from '././shared/guard/auth.guard';
+
+import { PageAccueilComponent } from '././components/page-accueil/page-accueil.component';
+import { MesGroupesComponent } from '././components/mes-groupes/mes-groupes.component';
+import { MesMessagesComponent } from '././components/mes-messages/mes-messages.component';
+import { AllGroupesPostComponent } from '././components/all-groupes-post/all-groupes-post.component';
+
 const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'home', component: PageAccueilComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent },
+  { path: 'mes-groupes', component: MesGroupesComponent},
+  { path: 'mes-messages', component: MesMessagesComponent},
+  { path: 'fil-actualite', component: AllGroupesPostComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,44 +3,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Firebase services + enviorment module
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+
+import { AuthService } from './shared/services/auth.service';
 
 import { AppComponent } from './app.component';
-import { AddGroupeComponent } from './components/add-groupe/add-groupe.component';
-import { AllGroupesPostComponent } from './components/all-groupes-post/all-groupes-post.component';
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { GroupeDetailsComponent } from './components/groupe-details/groupe-details.component';
-import { GroupesListComponent } from './components/groupes-list/groupes-list.component';
-import { LeftMenuComponent } from './components/left-menu/left-menu.component';
-import { MesGroupesComponent } from './components/mes-groupes/mes-groupes.component';
-import { MesMessagesComponent } from './components/mes-messages/mes-messages.component';
-import { PageAccueilComponent } from './components/page-accueil/page-accueil.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
+import { LeftMenuComponent } from './components/left-menu/left-menu.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { PageAccueilComponent } from './components/page-accueil/page-accueil.component';
+import { MesGroupesComponent } from './components/mes-groupes/mes-groupes.component';
+import { MesMessagesComponent } from './components/mes-messages/mes-messages.component';
+import { AllGroupesPostComponent } from './components/all-groupes-post/all-groupes-post.component';
+
+import { AddGroupeComponent } from './components/add-groupe/add-groupe.component';
+import { GroupeDetailsComponent } from './components/groupe-details/groupe-details.component';
+import { GroupesListComponent } from './components/groupes-list/groupes-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddGroupeComponent,
-    AllGroupesPostComponent,
     DashboardComponent,
-    ForgotPasswordComponent,
-    GroupeDetailsComponent,
-    GroupesListComponent,
-    LeftMenuComponent,
-    MesGroupesComponent,
-    MesMessagesComponent,
-    PageAccueilComponent,
     SignInComponent,
     SignUpComponent,
-    VerifyEmailComponent
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    LeftMenuComponent,
+    MenuComponent,
+    PageAccueilComponent,
+    MesGroupesComponent,
+    MesMessagesComponent,
+    AllGroupesPostComponent,
+    AddGroupeComponent,
+    GroupeDetailsComponent,
+    GroupesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
