4938c2ece54037c2f9221db8246bb4eb92bee68f
Nelchel <nelchel@riseup.net>
2021-03-14 15:24:06 +0100

    API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
     firebase: {
-    apiKey: "xxxxxxxx-xxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxx",
-    storageBucket: "xxxxxxxx",
-    messagingSenderId: "xxxxxx",
-    appId: "xxxxx",
-    measurementId: "xxxxxxxxxxxxxxxx"
+      apiKey: 'AIzaSyCpBll86ef4C-anE0tKRieL_sGMJQHfOUI',
+      authDomain: 'projet-2ea5d.firebaseapp.com',
+      databaseURL: 'https://projet-2ea5d-default-rtdb.firebaseio.com',
+      projectId: 'projet-2ea5d',
+      storageBucket: 'projet-2ea5d.appspot.com',
+      messagingSenderId: '908874767053',
+      appId: '1:908874767053:web:aab93ac0845117f5cca784',
+      measurementId: 'G-Q2FX7CT1LB'
   }
 };
 
