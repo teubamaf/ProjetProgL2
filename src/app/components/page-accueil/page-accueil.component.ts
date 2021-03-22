0d46ea23d6165d2bfa78408399303ed40972d91e
Nelchel <nelchel@riseup.net>
2021-03-22 14:19:11 +0100

    avant projet

--- src/app/components/page-accueil/page-accueil.component.ts
+++ src/app/components/page-accueil/page-accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-page-accueil',
@@ -7,7 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAccueilComponent implements OnInit {
 
-  constructor() { }
+  items: Observable<any[]>;
+  itemPosts: Observable<any[]>;
+  itemUsers: Observable<any[]>;
+
+  uid = this.authService.userData.uid;
+
+  constructor(
+    firestore: AngularFirestore,
+    public authService: AuthService
+  ) {
+    this.items = firestore.collection(`groupes`).valueChanges();
+    this.itemPosts = firestore.collection(`posts`).valueChanges();
+    this.itemUsers = firestore.collection(`users`).valueChanges();
+  }
 
   ngOnInit(): void {
   }
